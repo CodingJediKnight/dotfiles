49df091449e8b7d033c7497688a26563710e56cc
jedi <coding.jedi.knight@gmail.com>
2024-02-05 21:16:10 +0300

    (edit)[fish]{add aliases}

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,7 +20,37 @@ set -x BAT_STYLE "header-filename,header-filesize,grid"
 # export NNN_PLUG='f:finder;o:fzopen;p:mocq;d:diffs;t:nmount;v:imgview;i:preview-tui'
 # export NNN_FIFO=/tmp/nnn.fifo
 
-[ -e ~/.aliases ]; and . ~/.aliases
+alias tmux="env TERM=xterm-256color tmux"
+alias tmux-new="tmux new-session -d -s $USER"
+alias tmux-attach="tmux attach-session -t $USER"
+
+alias myip="dig myip.opendns.com @208.67.222.222"
+
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias +x="chmod +x"
+
+alias cdef='echo -e "\033]6;1;bg;*;default\a"'
+
+alias nnn="nnn -dHiUc -P i"
+
+alias l="exa --long --classify --group --icons --header"
+
+function ssh
+	command ssh $argv
+	echo -e "\033]6;1;bg;*;default\a"
+end
+
+function is_ssh
+	[ -n "$SSH_CONNECTION" ]; or [ -n "$SSH_CLIENT" ]; or [ -n "$SSH_TTY" ]
+end
+
+function is_tmux
+	[ -n "$TMUX" ]
+end
+
+function grep_highlight
+	grep --color=always -E "$argv[1]|\$" $argv[2];
+end
 
 function save_history --on-event fish_preexec
     history --save
