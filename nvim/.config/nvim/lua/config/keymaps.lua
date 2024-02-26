26a048f4aa03af5761194a3b67a922135d05c2ee
jedi <coding.jedi.knight@gmail.com>
2024-02-26 17:40:15 +0300

    feat(nvim): add go dap config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,66 +1,69 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local k = vim.keymap
 
 -- lsp
-vim.keymap.set("n", "<leader>S", "<cmd>LspStop<CR>", { desc = "LspStop" })
+k.set("n", "<leader>S", "<cmd>LspStop<CR>", { desc = "LspStop" })
 
 -- surrounding words
-vim.keymap.set("n", "<leader>wsq", 'ciw""<Esc>P', { desc = "Word Surround Quotes" })
+k.set("n", "<leader>wsq", 'ciw""<Esc>P', { desc = "Word Surround Quotes" })
 
 -- telescope symbols
-vim.keymap.set("n", "<leader>fs", "<cmd>Telescope symbols<cr>", { desc = "Find Symbols" })
+k.set("n", "<leader>fs", "<cmd>Telescope symbols<cr>", { desc = "Find Symbols" })
 
-vim.keymap.set("i", "<Char-0x4>", "<Del>")
-vim.keymap.set("i", "<C-K>", "<C-o>d$")
-vim.keymap.set("i", "<Char-0x1b><Char-0x7f>", "<C-o>db")
+k.set("i", "<Char-0x4>", "<Del>")
+k.set("i", "<C-K>", "<C-o>d$")
+k.set("i", "<Char-0x1b><Char-0x7f>", "<C-o>db")
 
-vim.keymap.set("i", "<Esc>b", "<C-o>b")
-vim.keymap.set("i", "<Esc>f", "<C-o>w")
+k.set("i", "<Esc>b", "<C-o>b")
+k.set("i", "<Esc>f", "<C-o>w")
 
 -- ThePrimeagen remaps
-vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
-vim.keymap.set("n", "<leader>u", "<cmd>UndotreeShow<CR>")
+k.set("n", "<leader>pv", "<cmd>Ex<CR>")
+k.set("n", "<leader>u", "<cmd>UndotreeShow<CR>")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+k.set("v", "J", ":m '>+1<CR>gv=gv")
+k.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+k.set("n", "<C-d>", "<C-d>zz")
+k.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "J", "mzJ`z")
+k.set("n", "n", "nzzzv")
+k.set("n", "N", "Nzzzv")
+k.set("n", "J", "mzJ`z")
 
-vim.keymap.set("x", "<leader>p", '"_dP')
+k.set("x", "<leader>p", '"_dP')
 
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>Y", '"+Y')
+k.set("n", "<leader>y", '"+y')
+k.set("v", "<leader>y", '"+y')
+k.set("n", "<leader>Y", '"+Y')
 
-vim.keymap.set("n", "<leader>d", '"_d')
-vim.keymap.set("v", "<leader>d", '"_d')
+k.set("n", "<leader>d", '"_d')
+k.set("v", "<leader>d", '"_d')
 
-vim.keymap.set("n", "<C-L>", ':nohl<CR><C-L>')
+k.set("n", "<C-L>", ':nohl<CR><C-L>')
 
 -- Prevent exiting on Control-z
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set({ "n", "v", "c" }, "<C-Z>", "<nop>")
+k.set("n", "Q", "<nop>")
+k.set({ "n", "v", "c" }, "<C-Z>", "<nop>")
 
-vim.keymap.set("n", "<C-F>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+k.set("n", "<C-F>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+k.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+k.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+k.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+k.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>")
+k.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+k.set("n", "<leader>x", "<cmd>!chmod +x %<CR>")
 
-vim.keymap.set({ "n", "v", "c", "i" }, "<F1>", ":set number!<CR> :set relativenumber!<CR>")
-vim.keymap.set({ "n", "v", "c", "i" }, "<F2>", ":set wrap!<CR>")
-vim.keymap.set({ "n", "v", "c", "i" }, "<F4>", ":set list!<CR>")
-vim.keymap.set({ "n", "v", "c", "i" }, "<F7>", ":set spell!<CR>")
+k.set({ "n", "v", "c", "i" }, "<F1>", ":set number!<CR> :set relativenumber!<CR>")
+k.set({ "n", "v", "c", "i" }, "<F2>", ":set wrap!<CR>")
+k.set({ "n", "v", "c", "i" }, "<F3>", ":set signcolumn=no<CR>")
+k.set({ "n", "v", "c", "i" }, "<F4>", ":set list!<CR>")
+k.set({ "n", "v", "c", "i" }, "<F5>", ":set signcolumn=yes<CR>")
+k.set({ "n", "v", "c", "i" }, "<F7>", ":set spell!<CR>")
 
 -- nvim go related
-vim.keymap.set("n", "<leader>gt", "<cmd>GoTest<CR>", { desc = "Go Test" })
+k.set("n", "<leader>gt", "<cmd>GoTest<CR>", { desc = "Go Test" })
--- nvim/.config/nvim/lua/plugins/go.lua
+++ nvim/.config/nvim/lua/plugins/go.lua
@@ -18,7 +18,14 @@ return {
         dependencies = "mfussenegger/nvim-dap",
         config = function()
             require("dapui").setup()
+            vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint'})
         end,
+        keys = {
+            {mode = "n", "<leader>dt", ":DapUiToggle<CR>", desc = "Dap Ui Toggle"},
+            {mode = "n", "<leader>db", ":DapToggleBreakpoint<CR>", desc = "Dap Toggle Breakpoint"},
+            {mode = "n", "<leader>dc", ":DapContinue<CR>", desc = "Dap Continue"},
+            {mode = "n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", desc = "Dap Reset"},
+        },
     },
     { "theHamsta/nvim-dap-virtual-text" },
     { "nvim-telescope/telescope-dap.nvim" },
